b20580ca86b35a8eba530198dd601412d4c98f97
MarlonDSantos <marlonmx5@hotmail.com>
2022-06-03 14:26:11 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const jump = () => {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    //IF representa o Game-Over do Mario
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        //mario.src = './imagens/gameover.png'
+
+        clearInterval(loop); //Encerra loop caso dar Game-Over
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
